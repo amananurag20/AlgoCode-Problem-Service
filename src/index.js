e63b110714cab28774baf24f224f78037e39861b
aman anurag <amananurag.20@gmail.com>
2025-02-28 16:23:25 +0530

    Fix: changed variable naming to camelCase

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const { PORT } = require('./config/server.config');
+const { PORT } = require("./config/server.config");
+const apiRouter = require("./routes");
 
 const app = express();
 
@@ -9,12 +10,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 
+// If any request comes and route starts with /api, we map it to apiRouter
+app.use("/api", apiRouter);
 
-app.get('/ping', (req, res) => {
-    return res.json({message: 'Problem Service is alive'});
+app.get("/ping", (req, res) => {
+  return res.json({ message: "Problem Service is alive" });
 });
 
-
 app.listen(PORT, () => {
-    console.log(`Server started at PORT: ${PORT}`);
+  console.log(`Server started at PORT: ${PORT}`);
 });
